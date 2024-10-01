918139408d13c337a74748a9cc5a5399c5377dd0
Dromader2137 <wojciechowski.ignacy@proton.me>
2024-10-01 15:15:37 +0200

    update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,18 +1,8 @@
 if status is-interactive
 	starship init fish | source
-	
 	set fish_greeting
-
-	export QT_QPA_PLATFORMTHEME=qt5ct
-	export RANGER_LOAD_DEFAULT_RC=false
 	export EDITOR="nvim"
 	export VISUAL="nvim"
-	
-	gsettings set org.gnome.desktop.interface gtk-theme 'gruvbox-dark-gtk'
-	gsettings set org.gnome.desktop.interface icon-theme 'gruvbox-dark-icons-gtk'
-	gsettings set org.gnome.desktop.interface cursor-theme 'Simp1e-Gruvbox-Dark'
-	gsettings set org.gnome.desktop.interface font-name 'Mononoki Nerd Font 11'  
-	
 	alias ls="exa -l --group-directories-first -s Extension"
 	alias la="exa -la --group-directories-first -s Extension"
 	alias ll="exa -la --group-directories-first -s Extension -T -L 2"
@@ -24,7 +14,11 @@ if status is-interactive
 	alias gpa="git push --all"
 	alias gl="git log --graph --oneline --all"
 	alias gsw="git switch"
-	
+	alias gr="git restore"
+	alias gd="git diff"
+	alias gcb="git checkout -b"
+	alias gbm="git brach -m"
+	alias disk="lsblk -a -o NAME,SIZE,FSUSED,TYPE,FSTYPE,MOUNTPOINTS"
 	contains $HOME/.cargo/bin/ $fish_user_paths; or set -Ua fish_user_paths $HOME/.cargo/bin/
 	contains $HOME/.local/bin/ $fish_user_paths; or set -Ua fish_user_paths $HOME/.local/bin/
 end
