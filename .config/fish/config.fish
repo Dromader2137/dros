2bac16652ccd3d39b3101c5e087de15aee0fc025
Dromader2137 <wojciechowski.ignacy@proton.me>
2026-05-05 12:21:00 +0200

    format

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,41 +7,41 @@ if status is-login
 end
 
 if status is-interactive
-    starship init fish | source
+	starship init fish | source
 
-    set fish_greeting
+	set fish_greeting
 
-    export EDITOR="nvim"
-    export VISUAL="nvim"
+	export EDITOR="nvim"
+	export VISUAL="nvim"
 
 	alias nv="nvim"
 
-    alias ls="exa -l --group-directories-first -s Extension"
-    alias la="exa -la --group-directories-first -s Extension"
-    alias ll="exa -l --group-directories-first -s Extension -T -L 3"
-    alias lla="exa -la --group-directories-first -s Extension -T -L 2"
+	alias ls="exa -l --group-directories-first -s Extension"
+	alias la="exa -la --group-directories-first -s Extension"
+	alias ll="exa -l --group-directories-first -s Extension -T -L 3"
+	alias lla="exa -la --group-directories-first -s Extension -T -L 2"
 
-    alias gs="git status"
-    alias ga="git add"
-    alias gc="git commit -m"
-    alias gca="git commit -a -m"
-    alias gf="git fetch"
-    alias gpl="git pull"
-    alias gp="git push"
-    alias gl='git log --color --graph --pretty=format:'\''%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset'\'' --abbrev-commit'
-    alias gla='git log --all --color --graph --pretty=format:'\''%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset'\'' --abbrev-commit'
-    alias gsw="git switch"
-    alias gr="git restore"
-    alias gd="git diff"
-    alias gcb="git checkout -b"
-    alias gbm="git branch -m"
-    alias gbd="git branch -D"
+	alias gs="git status"
+	alias ga="git add"
+	alias gc="git commit -m"
+	alias gca="git commit -a -m"
+	alias gf="git fetch"
+	alias gpl="git pull"
+	alias gp="git push"
+	alias gl='git log --color --graph --pretty=format:'\''%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset'\'' --abbrev-commit'
+	alias gla='git log --all --color --graph --pretty=format:'\''%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset'\'' --abbrev-commit'
+	alias gsw="git switch"
+	alias gr="git restore"
+	alias gd="git diff"
+	alias gcb="git checkout -b"
+	alias gbm="git branch -m"
+	alias gbd="git branch -D"
 	alias gm="git merge"
 
-    alias disk="lsblk -a -o NAME,SIZE,FSUSED,TYPE,FSTYPE,MOUNTPOINTS"
+	alias disk="lsblk -a -o NAME,SIZE,FSUSED,TYPE,FSTYPE,MOUNTPOINTS"
 
-    alias wificu="nmcli d w connect --ask"
-    alias wifick="nmcli d w connect"
-    alias nets="nmcli"
-    alias pip="curl ip.me"
+	alias wificu="nmcli d w connect --ask"
+	alias wifick="nmcli d w connect"
+	alias nets="nmcli"
+	alias pip="curl ip.me"
 end
